8ebba679304491c6b861748aa4c9fd8302a2a230
hansika vishwanath <vishwanathhansika@gmail.com>
2025-11-16 09:39:35 +0530

    python program to print numbers in given range

---
+++ print1to100.py
@@ -0,0 +1,4 @@
+def print_numbers(start,end):
+ for a in range(start,end+1):
+  print (a)
+print_numbers(10,15)
---
+++ pythonprog.py
@@ -0,0 +1,18 @@
+"""tup=(21,36,14,24)
+print(tup)
+
+data={1:'navin',2:'kiran',3:'harshith'}
+print(data.get(6))
+
+keys=['navin','kiran','harsh']
+values=['c','python','java']
+data= dict(zip(keys,values))
+#print(data)
+#print(data('monika'))
+data['monika']='cs'
+print(data)
+#del data['harsh']
+"""
+if false:
+    print("iam right")
+     
